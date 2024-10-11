e29808efec27b2d72be4ded491ef3dc5021db6a6
Josh K Laptop <joshuafkaplan@gmail.com>
2024-10-10 22:29:36 -0400

    finished base mobile styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    
+    extend: {
+      fontFamily: {
+        'barlow': ['Barlow Semi Condensed', 'sans-serif'],
+      },
+      colors: {
+        // Primary
+        moderateViolet: 'hsl(263, 55%, 52%)',
+        veryDarkGrayishBlue: 'hsl(217, 19%, 35%)',
+        veryDarkBlackishBlue: 'hsl(219, 29%, 14%)',
+  
+        // Neutral
+        lightGray: 'hsl(0, 0 %, 81 %)',
+        lightGrayishBlue: 'hsl(210, 46 %, 95 %)',
+      },
+      fontSize: {
+        mxsm: '0.813rem',
+      },
+    },
   },
   plugins: [],
 }
